5503b90ab63f2a2bc58cb2e04124f78abc03a070
Fernanda Martins <71784710+chatassaura@users.noreply.github.com>
2022-04-08 19:56:36 -0300

    Add files via upload

---
+++ CadastroPessoas/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CadastroPessoas
+{
+    partial class frm_cadastro_aluno
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.btn_anterior = new System.Windows.Forms.Button();
+            this.btn_proximo = new System.Windows.Forms.Button();
+            this.lbl_nome_aluno = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Font = new System.Drawing.Font("Papyrus", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome.Location = new System.Drawing.Point(61, 9);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(63, 24);
+            this.lbl_nome.TabIndex = 0;
+            this.lbl_nome.Text = "Nome: ";
+            // 
+            // btn_anterior
+            // 
+            this.btn_anterior.Font = new System.Drawing.Font("Papyrus", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_anterior.Location = new System.Drawing.Point(30, 51);
+            this.btn_anterior.Name = "btn_anterior";
+            this.btn_anterior.Size = new System.Drawing.Size(120, 38);
+            this.btn_anterior.TabIndex = 1;
+            this.btn_anterior.Text = "<< Anterior";
+            this.btn_anterior.UseVisualStyleBackColor = true;
+            this.btn_anterior.Click += new System.EventHandler(this.btn_anterior_Click);
+            // 
+            // btn_proximo
+            // 
+            this.btn_proximo.Font = new System.Drawing.Font("Papyrus", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_proximo.Location = new System.Drawing.Point(156, 51);
+            this.btn_proximo.Name = "btn_proximo";
+            this.btn_proximo.Size = new System.Drawing.Size(120, 38);
+            this.btn_proximo.TabIndex = 2;
+            this.btn_proximo.Text = "Próximo >>";
+            this.btn_proximo.UseVisualStyleBackColor = true;
+            this.btn_proximo.Click += new System.EventHandler(this.btn_proximo_Click);
+            // 
+            // lbl_nome_aluno
+            // 
+            this.lbl_nome_aluno.AutoSize = true;
+            this.lbl_nome_aluno.Font = new System.Drawing.Font("Papyrus", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome_aluno.Location = new System.Drawing.Point(130, 9);
+            this.lbl_nome_aluno.Name = "lbl_nome_aluno";
+            this.lbl_nome_aluno.Size = new System.Drawing.Size(123, 24);
+            this.lbl_nome_aluno.TabIndex = 3;
+            this.lbl_nome_aluno.Text = "lbl_nome_aluno";
+            // 
+            // frm_cadastro_aluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 101);
+            this.Controls.Add(this.lbl_nome_aluno);
+            this.Controls.Add(this.btn_proximo);
+            this.Controls.Add(this.btn_anterior);
+            this.Controls.Add(this.lbl_nome);
+            this.Name = "frm_cadastro_aluno";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Button btn_anterior;
+        private System.Windows.Forms.Button btn_proximo;
+        private System.Windows.Forms.Label lbl_nome_aluno;
+    }
+}
+
---
+++ CadastroPessoas/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroPessoas
+{
+    public partial class frm_cadastro_aluno : Form
+    {
+        String[] nomes = new string[5];
+        int contador = 0;
+
+        public frm_cadastro_aluno()
+        {
+            InitializeComponent();
+
+            nomes[0] = "Gustavo";
+            nomes[1] = "Fernanda";
+            nomes[2] = "Neto";
+            nomes[3] = "Vitor";
+            nomes[4] = "Otávio";
+            lbl_nome_aluno.Text = nomes[0];
+        }
+
+        private void btn_anterior_Click(object sender, EventArgs e)
+        {
+
+            if (contador > 0)
+            {
+                contador--;
+                lbl_nome_aluno.Text = nomes[contador];
+            }
+
+        }
+
+        private void btn_proximo_Click(object sender, EventArgs e)
+        {
+            if (contador < 4)
+            {
+                contador++;
+                lbl_nome_aluno.Text = nomes[contador];
+            }
+        }
+    }
+}
